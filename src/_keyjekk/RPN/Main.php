88fb9b5047a25252bbe7b1ce92431060b6582f34
Keyjekk <108344123+Keyjekk@users.noreply.github.com>
2022-11-20 21:47:05 +0300

    Update Main.php

--- src/_keyjekk/RPN/Main.php
+++ src/_keyjekk/RPN/Main.php
@@ -1,1 +1,20 @@
+<?php
 
+declare(strict_types=1);
+
+namespace _keyjekk\RPN;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener{
+
+  public function onEnable():void{
+
+  $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+  $this->getLogger()->info("§dRPN was started!");
+
+  }
+
+}
