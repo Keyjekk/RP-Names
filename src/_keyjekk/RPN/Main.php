b75749250f5b8b8b6ed404ce16369f62ae3799cc
Keyjekk <108344123+Keyjekk@users.noreply.github.com>
2022-11-21 12:04:12 +0300

    update Main.php

--- src/_keyjekk/RPN/Main.php
+++ src/_keyjekk/RPN/Main.php
@@ -20,9 +20,21 @@ class Main extends PluginBase implements Listener{
   public function onEnable():void{
 
   $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  
+  $this->names = new Config($this->getDataFolder()."names.json", CONFIG::JSON, ["version" => 1]);
 
   $this->getLogger()->info("§dRPN was started!");
 
+  }
+  
+  public function onJoin(PlayerJoinEvent $ev){
+    
+    $nick = $ev->getPlayer()->getName();
+    
+    if(!isset($this->names->get($nick))){
+      $this->names->set($nick, "No Name");
+    }
+    
   }
   
   public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args):bool{
